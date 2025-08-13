9949016aa0330a061bda1069835675a3c6f0d938
Jedizhi <jedizhi@gmail.com>
2025-08-13 10:21:06 +0300

    Update streamlit_app.py
    updated data source to google spreadsheet

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,13 @@ import plotly.express as px
 from streamlit_gsheets import GSheetsConnection
 
 # Connect to google spreadsheets
-#url = "https://docs.google.com/spreadsheets/d/1Iyau6bAAdbVz9iVYA2ai5OQZNs4mRbSly0Mo6YEo6Rk/edit?usp=sharing"
+url = "https://docs.google.com/spreadsheets/d/1oGcvcMnjVTynDW-VjL58ssUaVVrlqWwt8AKtyLiDykk/edit?gid=0#gid=0"
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 # Load data
 # df = pd.read_csv("SHC Heavy Equipment Master Log.csv")
-df = conn.read(worksheet = Sheet1)
-st.dataframe(df)
+df = conn.read(spreadsheet = url)
+#st.dataframe(df)
 
 st.title("📆 JGCP-HE Document Expiry Status - PH III")
 
