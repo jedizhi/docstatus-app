8db0a6f31d7822c8df831aecdcc7555b66bd5bff
Jedizhi <jedizhi@gmail.com>
2026-02-15 13:40:29 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ from streamlit_gsheets import GSheetsConnection
 # PAGE CONFIG
 # =====================
 st.set_page_config(
-    page_title="JGCP-HE Document Expiry Dashboard",
+    page_title="Heavy Equipment/Vehicles Document Expiry Dashboard",
     page_icon="📆",
     layout="wide",
     initial_sidebar_state="expanded"
